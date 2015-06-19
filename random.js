9fd9a3401944ad488fc2502d0a55c75d4d5ff7ac
Carlos <cfigueroa@gmail.com>
2015-06-19 12:41:24 +0200

    Incluida nueva condicion

--- random.js
+++ random.js
@@ -1,8 +1,11 @@
 var numero=Math.random();
 
-if (numero <= 0.5){
+if (numero < 0.5){
 	console.log("\n"+ numero +" MENOR que 0.5\n");
 }
-else {
+else if (numero > 0.5){
 	console.log("\n"+ numero +" MAYOR que 0.5\n");
+}
+else {
+	console.log("\n"+ numero +" IGUAL a 0.5\n");
 }
